dfb34753d1e9c55c83df6e297180443857e1b7b4
iainwoodou <iain.wood@open.ac.uk>
2022-10-13 13:46:23 +0000

    Update 4 files

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,57 @@
 <template>
-  <HelloWorld />
+  <select id="language-select" v-model="locale">
+    <option value="zh">简体中文</option>
+    <option value="en">English</option>
+    <option value="ptBR">Brazilian Portuguese</option>
+  </select>
+  <div id="mmcontainer">
+    <mindmap
+      v-model="mm"
+      :edit="true"
+      :drag="true"
+      :x-gap="84"
+      :y-gap="18"
+      :branch="5"
+      :zoom="true"
+      :center-btn="true"
+      :fit-btn="true"
+      :add-node-btn="true"
+      :download-btn="true"
+      :sharp-corner="false"
+      :ctm="true"
+      :locale="en"
+      :timetravel="true"
+    ></mindmap>
+  </div>
 </template>
 
 <script>
-import HelloWorldVue from "./components/HelloWorld.vue";
+import mindmap from "vue3-mindmap";
+import "@/mmstyle.css";
+
 export default {
   name: "App",
   components: {
-    HelloWorld: HelloWorldVue
+    mindmap
+  },
+  data() {
+    return {
+      locale: "en",
+      mm: [
+        {
+          name: "Start"
+        }
+      ]
+    };
   }
 };
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#mmcontainer {
+  position: absolute;
+  left: 0px;
+  right: 0px;
+  top: 60px;
+  bottom: 0px;
 }
-
 </style>
